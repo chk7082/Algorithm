885f372b515d25ee8fd0bda1b7bd9c41e14776af
chk7082 <whgusrl7082@gmail.com>
2023-03-28 14:14:37 +0900

    minor update 0328

--- Course/SW문제해결응용 Combination & Greedy Algorithm/maximum_prize_money.py
+++ Course/SW문제해결응용 Combination & Greedy Algorithm/maximum_prize_money.py
@@ -1,63 +1,3 @@
-# def maximum_prize_money(nums, change_num):
-#     '''
-#     function that return the maximum winning prize money
-#     that could be made by exactly swapping change_num times
-#     '''
-#
-#     # pre-compute the sorted version
-#     sorted_nums = sorted(nums, reverse=True)
-#     L = len(nums)
-#
-#     # check if it has digits with multiplicity >= 2
-#     if len(set(nums)) < L:
-#         can_stay = True
-#     else:
-#         can_stay = False
-#
-#     # swap it (change_num) - times
-#     for _ in range(change_num):
-#         swap(nums, sorted_nums, L, can_stay)
-#
-#     # reconnect them
-#     return int(''.join(nums))
-#
-#
-# def swap(nums, sorted_nums, L, can_stay):
-#     '''
-#     one step of swapping
-#     '''
-#
-#     # compare the given two list element-wisely
-#     # it is sufficient to check it up to L-2
-#     for i in range(L-1):
-#         # if those two are different
-#         if nums[i] != sorted_nums[i]:
-#             index_different = i
-#             break
-#
-#     # for-else, when it's already optimal
-#     else:
-#         # when we could just swap the same digit
-#         if can_stay:
-#             return
-#         else:
-#             # swap the last two ones
-#             nums[-2], nums[-1] = nums[-1], nums[-2]
-#             return
-#
-#     # find the index in nums that have value sorted_nums[index_different]
-#     # when tie, use the largest one
-#     for j in range(L-1, -1, -1):
-#         if nums[j] == sorted_nums[index_different]:
-#             # remember it
-#             index_to_swap = j
-#             break
-#
-#     # swap them
-#     nums[index_to_swap], nums[index_different] = nums[index_different], nums[index_to_swap]
-#     return
-
-
 def f(cur_num, change_num, can_stay):
     if cur_num == change_num:
         record[0] = max(record[0], ''.join(nums))
@@ -137,7 +77,7 @@ for t in range(1, T+1):
             record[0] = ''.join(sorted_nums)
         else:
             is_even = is_even_permutation(nums, sorted_nums, L)
-            if is_even:
+            if (is_even and change_num%2==0) or (not is_even and change_num%2):
                 record[0] = ''.join(sorted_nums)
             else:
                 sorted_nums[-1], sorted_nums[-2] = sorted_nums[-2], sorted_nums[-1]
